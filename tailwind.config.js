63268cc010241fc7526a5b92d4485d2237e3b1f8
Kapri27 <paumau27@gmail.com>
2025-10-02 06:41:48 -0300

    Inicio: formulario para practicar tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+ /** @type {import('tailwindcss').Config} */
+ export default {
+   content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        colors: {
+          'azul-claro': '#243cff',
+          'azul-oscuro': '#0d1b3e',
+        },
+        width:{
+          '42' : '270px',
+        }
+      },
+    },
+    plugins: [],
+  }
