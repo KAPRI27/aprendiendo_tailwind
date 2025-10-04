2401e2aaa1305aa93543c9c361aa2a8aaa952712
Kapri27 <paumau27@gmail.com>
2025-10-04 07:10:54 -0300

    Style: Aplica medidas responsive y crea otra

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,17 @@
    content: ["./src/**/*.{html,js}"],
     theme: {
       extend: {
+        screens:{
+          'tablet': '900px'
+        },
         colors: {
           'azul-claro': '#243cff',
           'azul-oscuro': '#0d1b3e',
         },
         width:{
           '42' : '270px',
-        }
+        },
+        
       },
     },
     plugins: [],
